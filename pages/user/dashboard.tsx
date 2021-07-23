cf7a5e4278c3e1cf775c8a095ebab82097e7551d
Cotetommy <nordik.tommy+1@gmail.com>
2021-07-23 16:49:13 -0400

    progress

--- pages/user/dashboard.tsx
+++ pages/user/dashboard.tsx
@@ -2,6 +2,29 @@ import React, {useContext, useEffect, useState} from "react";
 import {client, hostedFields} from "braintree-web"
 import Axios from "axios";
 import {Props, UserContext} from "../../components/userContext/user-context";
+import {GetServerSideProps} from "next";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    console.log(ctx.req)
+    const isLogged = await ctx.req.headers['isLogged'];
+
+    if(!isLogged){
+        return{
+            redirect:{
+                destination: '/user/login', //usually the login page
+                permanent: false,
+            }
+        }
+    }
+
+    return{
+        props: {
+            authenticated: true
+        }
+    }
+
+    // return { props: { logged: false } }
+}
 
 const Dashboard: React.FC = () => {
     const [loading, setLoading] = useState<boolean>(false)
@@ -41,17 +64,9 @@ const Dashboard: React.FC = () => {
                     "access-token": headers["access-token"]
                 }
             })
-        console.log(payment_res.data.length > 0, payment_res.data)
-        console.log(Axios.get(`${process.env.NEXT_PUBLIC_API_URL}${userState.channel}/subscribers/${userState.user['id']}/payment_methods`,
-            {
-                params: {
-                    uid: headers['uid'],
-                    client: headers['client'],
-                    "access-token": headers["access-token"]
-                }
-            }))
+        console.log(payment_res.data?.length && payment_res.data.length > 0, payment_res.data)
         setPaymentMethods(payment_res.data)
-        return response.data.client_token
+        return response && response.data.client_token
     }
 
     const instance = (token: string) => {
@@ -79,24 +94,30 @@ const Dashboard: React.FC = () => {
                     "access-token": headers["access-token"]
                 }
             })
-        if (payment_res.data.length > 0) {
-            await Axios.put(`${process.env.NEXT_PUBLIC_API_URL}${userState.channel}/subscribers/${userState.user['id']}/payment_methods/null`,
-                {
-                    nonce_from_the_client: nonce,
-                    uid: headers['uid'],
-                    client: headers['client'],
-                    "access-token": headers["access-token"]
-                }).then(r => console.log(r))
-            setLoading(false)
-        } else {
-            await Axios.post(`${process.env.NEXT_PUBLIC_API_URL}${userState.channel}/subscribers/${userState.user['id']}/payment_methods`,
-                {
-                    nonce_from_the_client: nonce,
-                    uid: headers['uid'],
-                    client: headers['client'],
-                    "access-token": headers["access-token"]
-                }).then(r => console.log(r))
-            setLoading(false)
+        if (userState.user['id'] !== null) {
+            if (payment_res.data.length > 0) {
+                await Axios.put(`${process.env.NEXT_PUBLIC_API_URL}${userState.channel}/subscribers/${userState.user['id']}/payment_methods/null`,
+                    {
+                        nonce_from_the_client: nonce,
+                        uid: headers['uid'],
+                        client: headers['client'],
+                        "access-token": headers["access-token"]
+                    })
+                    .then(r => console.log(r))
+                    .catch(err => userDispatch({type: 'ERROR'}))
+                setLoading(false)
+            } else {
+                await Axios.post(`${process.env.NEXT_PUBLIC_API_URL}${userState.channel}/subscribers/${userState.user['id']}/payment_methods`,
+                    {
+                        nonce_from_the_client: nonce,
+                        uid: headers['uid'],
+                        client: headers['client'],
+                        "access-token": headers["access-token"]
+                    })
+                    .then(r => console.log(r))
+                    .catch(err => userDispatch({type: 'ERROR'}))
+                setLoading(false)
+            }
         }
         setAlert(true)
     }
@@ -190,39 +211,49 @@ const Dashboard: React.FC = () => {
                         <div className="card shadow-2xl lg:card-side bg-12dp text-primary-content">
                             <div className="card-body">
                                 <p className="text-xl">Payment Method</p>
-                                <form id="cardForm">
-                                    <label htmlFor="card-number"
-                                           className={error?.includes('number') ? 'text-error' : ''}>
-                                        Card Number</label>
-                                    <div id="card-number" className="hosted-field mt-3"/>
-                                    <label htmlFor="expiration-date"
-                                           className={error?.includes('expirationDate') ? 'text-error' : ''}>
-                                        Expiration Date</label>
-                                    <div id="expiration-date" className="hosted-field mt-3"/>
-                                    <label htmlFor="expiration-date"
-                                           className={error?.includes('cvv') ? 'text-error' : ''}>CVV</label>
-                                    <div id="cvv" className="hosted-field mt-3"/>
-                                    <div className='text-center mt-3'>
-                                        {alert &&
-                                        <div className="alert alert-success fixed my-auto">
-                                            <div className="flex-1">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                     stroke="#2196f3"
-                                                     className="w-6 h-6 mx-2">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                                                </svg>
-                                                <label>Success!</label>
-                                            </div>
-                                        </div>
-                                        }
-                                        <button className={loading ? "btn btn-primary loading" : "btn btn-primary"}
-                                                type='submit'>
-                                            {payment_methods.length > 0 ? 'Update' : 'Create'}
-                                        </button>
+                                <div className="collapse w-96 rounded-box border border-base-300 collapse-arrow">
+                                    <input type="checkbox"/>
+                                    <div className="collapse-title text-xl font-medium">
+                                        {payment_methods ? "Edit Payment Method" : "Add a Payment Method"}
                                     </div>
-                                </form>
-                                handleTestPlan
+                                    <div className="collapse-content">
+                                        <form id="cardForm">
+                                            <label htmlFor="card-number"
+                                                   className={error?.includes('number') ? 'text-error' : ''}>
+                                                Card Number</label>
+                                            <div id="card-number" className="hosted-field mt-3"/>
+                                            <label htmlFor="expiration-date"
+                                                   className={error?.includes('expirationDate') ? 'text-error' : ''}>
+                                                Expiration Date</label>
+                                            <div id="expiration-date" className="hosted-field mt-3"/>
+                                            <label htmlFor="expiration-date"
+                                                   className={error?.includes('cvv') ? 'text-error' : ''}>CVV</label>
+                                            <div id="cvv" className="hosted-field mt-3"/>
+                                            <div className='text-center mt-3'>
+                                                {alert &&
+                                                <div className="alert alert-success fixed my-auto">
+                                                    <div className="flex-1">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                             viewBox="0 0 24 24"
+                                                             stroke="#2196f3"
+                                                             className="w-6 h-6 mx-2">
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth="2"
+                                                                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                                        </svg>
+                                                        <label>Success!</label>
+                                                    </div>
+                                                </div>
+                                                }
+                                                <button
+                                                    className={loading ? "btn btn-primary loading" : "btn btn-primary"}
+                                                    type='submit'>
+                                                    {payment_methods ? 'Update' : 'Create'}
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="card shadow-2xl lg:card-side bg-12dp text-primary-content ml-5">
