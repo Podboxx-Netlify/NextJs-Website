c57a5fee91a6b29d5e61e8c338bc785430b25806
Cotetommy <nordik.tommy+1@gmail.com>
2021-07-29 17:11:10 -0400

    sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
-module.exports = {
-  reactStrictMode: true,
-  target: 'serverless',
-  images: {
-    domains: ['podboxx-production.s3.amazonaws.com', 'podboxx-development.s3.amazonaws.com', 'next.podboxx.com', 'podboxx.com', 'localhost'],
-  },
-}
+const {withSentryConfig} = require('@sentry/nextjs');
+
+const moduleExports = {
+    // Your existing module.exports
+    // module.exports = {
+    reactStrictMode: true,
+    target: 'serverless',
+    images: {
+        domains: ['podboxx-production.s3.amazonaws.com', 'podboxx-development.s3.amazonaws.com', 'next.podboxx.com', 'podboxx.com', 'localhost'],
+    },
+    // }
+};
+
+const SentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
